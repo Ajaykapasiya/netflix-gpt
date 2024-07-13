2759212f3b21f14975218dce2ce84fb868498441
ajay <ajaykumar290904@gmail.com>
2024-07-13 09:53:36 +0530

    Add some component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
-    return (<div className="text-3xl font-bold text-green-600">Hello everyone this is the netflix gpt for education purpose only </div>);
+    return <Body />;
 }
 
 export default App;
